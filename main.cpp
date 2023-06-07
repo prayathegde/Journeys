375b827d76f03adf08bf4a2bdc8d9859d88db6c8
prayathegde <prayat2090@gmail.com>
2023-06-07 00:58:54 -0400

    add test cases

--- main.cpp
+++ main.cpp
@@ -9,14 +9,53 @@ int main ()
         {'c', 'b'},
         {'c','d'},
         {'d','c'},
+        {'d','c'},
         {'d','b'},
         {'b','d'},
         {'d','e'},
     };
+
     Journeys j(paths);
-    auto rt = j.isValidJourney("abde");
-    cout << "Is Valid: " << rt << endl;
-    rt = j.isValidJourney("abcdbcdbcde");
-    cout << "Is Valid: " << rt << endl;
+
+    const char *path1 = "abde";
+    auto rt = j.isValidJourney(path1);
+    cout << "Is " << path1 << " Valid: " << rt << endl;
+
+    const char *path2 = "abcdbcdbcde";
+    rt = j.isValidJourney(path2);
+    cout << "Is " << path2 << " Valid: " << rt << endl;
+
+    const char *path3 = "abde";
+    rt = j.isValidJourney(path3);
+    cout << "Is " << path3 << " Valid: " << rt << endl;
+
+    const char *path4 = "abdbde";
+    rt = j.isValidJourney(path4);
+    cout << "Is " << path4 << " Valid: " << rt << endl;
+
+
+    const char *path5 = "abe";
+    rt = j.isValidJourney(path5);
+    cout << "Is " << path5 << " Valid: " << rt << endl;
+
+    const char *path6 = "aba";
+    rt = j.isValidJourney(path6);
+    cout << "Is " << path6 << " Valid: " << rt << endl;
+    
+    auto count = j.getTotalNumJourneys('a', 'e');
+    cout << "Total Number of Paths between ('a', 'e') " << count << endl;
+
+    count = j.getTotalNumJourneys('a', 'b');
+    cout << "Total Number of Paths between ('a', 'b') " << count << endl;
+
+    count = j.getTotalNumJourneys('b', 'e');
+    cout << "Total Number of Paths between ('b', 'e') " << count << endl;
+
+    count = j.getTotalNumJourneys('b', 'd');
+    cout << "Total Number of Paths between ('b', 'd') " << count << endl;
+
+    count = j.getTotalNumJourneys('b', 'z');
+    cout << "Total Number of Paths between ('b', 'z') " << count << endl;
+
     return 0;
 }
